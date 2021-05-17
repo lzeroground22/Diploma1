cd0e9d7571a0d962ee149419614af45c45b63160
Alex <User@Big-PC.les.local>
2021-05-17 23:41:45 +0300

    'link uploader' manual mode working

--- main.py
+++ main.py
@@ -2,42 +2,54 @@ import requests
 from pprint import pprint
 
 
-# class YaUploader:
-#     def __init__(self, token: str):
-#         self.token = token
-#
-#     def get_headers(self):
-#         return {
-#             'Content-type': 'application/json',
-#             'Authorization': 'OAuth {}'.format(self.token)
-#         }
-#
-#     def adr_parser(self, file_path):
-#         """Функция получает имя файла из пути к нему"""
-#         # adr = file_path
-#         adr_split = file_path.replace("\\", " ")
-#         adr_list = adr_split.split()
-#         # filename = adr_list[-1]
-#         return adr_list[-1]
-#
-#     def get_upload_link(self, disk_file_path):
-#         """Получаем ссылку для загрузки файла на Я.Диск"""
-#         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-#         headers = self.get_headers()
-#         params = {'path': disk_file_path, 'overwrite': 'true'}
-#         response = requests.get(upload_url, headers=headers, params=params)
-#         return response.json()
-#
-#     def upload(self, file_path):
-#         """Метод загруджает файл file_path на Я.Диск"""
-#         filename = self.adr_parser(file_path)
-#         href = self.get_upload_link(disk_file_path=filename).get('href', '')
-#         response = requests.put(href, data=open(file_path, 'rb'))
-#         response.raise_for_status()
-#         if response.status_code == 201:
-#             return f'File "{self.adr_parser(file_path)}" was created successfully'
-#
-#
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def adr_parser(self, file_path):
+        """Функция получает имя файла из пути к нему"""
+        # adr = file_path
+        adr_split = file_path.replace("\\", " ")
+        adr_list = adr_split.split()
+        # filename = adr_list[-1]
+        return adr_list[-1]
+
+    def get_upload_link(self, disk_file_path, src=None):
+        """Получаем ссылку для загрузки файла на Я.Диск"""
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        if src is None:
+            params = {'path': disk_file_path, 'overwrite': 'true'}
+        else:
+            params = {'url': src, 'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(upload_url, headers=headers, params=params)
+        return response.json()
+
+    def upload(self, file_path):
+        """Метод загруджает файл file_path на Я.Диск"""
+        filename = self.adr_parser(file_path)
+        href = self.get_upload_link(disk_file_path=filename).get('href', '')
+        response = requests.put(href, data=open(file_path, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            return f'File "{self.adr_parser(file_path)}" was created successfully'
+
+    def link_upload(self, src, disk_file_path):
+        upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        headers = self.get_headers()
+        # like_name = VkUser.decomposer('profile')
+        params = {'path': disk_file_path, 'url': src, 'disable_redirects': 'false'}
+        response = requests.post(upload_url, headers=headers, params=params)
+        # print(response)
+        response.raise_for_status()
+        if response.status_code == 202:
+            return f'File was created successfully'
 
 
 class VkUser:
@@ -94,13 +106,12 @@ class VkUser:
 
 
 if __name__ == '__main__':
-    # uploader = YaUploader('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
+    uploader = YaUploader('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
     # result = uploader.upload("D:\Python\Disk_file.txt")
-    # print(result)
-    vk_client = VkUser('bcb85bc9785a2be339acfd4e498c71a942a5414d56be54a29993f801ce84435ee7a237cd67709e0fe6d05', '5.130')
-    print(vk_client.decomposer('profile'))
-
+    result = uploader.link_upload('https://sun9-54.userapi.com/impf/HJ0UJYDpADxp1CKD-wmZ8DsEPBugvgTot1smKA/cbmFvAo3bhM.jpg?size=600x797&quality=96&sign=b1034a89f34892cc6b8b139e638e0c24&c_uniq_tag=6g8QxdJtyf5OKfuff460o5VIksDQLyjAhqQOSDooY6g&type=album', '/Neto/50.jpg')
+    # vk_client = VkUser('300f6b5d1cfe5dcd44fe9346ff2ce2efc37128d84986afffc85797438a6ff48402983dcb75bcba4501257', '5.130')
+    # print(vk_client.decomposer('profile'))
 
 # 'a7885b2a0024c9e139618ca0ff5be5688dd12f285dcb522ccfe6f0eacc76b59ac5fd6ddc2a89e354acfa0' frunde token
-#  'bcb85bc9785a2be339acfd4e498c71a942a5414d56be54a29993f801ce84435ee7a237cd67709e0fe6d05' photo token
+#  '300f6b5d1cfe5dcd44fe9346ff2ce2efc37128d84986afffc85797438a6ff48402983dcb75bcba4501257' photo token
 # https://oauth.vk.com/authorize?client_id=7845912&display=page&scope=photos&response_type=token&v=5.130
