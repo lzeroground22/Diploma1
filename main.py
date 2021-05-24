f081cc36fcfd0863a2f6a657a1ddbc37be6af6ee
Alex <User@Big-PC.les.local>
2021-05-24 16:49:31 +0300

    Ready version

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-from pprint import pprint
 
 
 class YaUploader:
@@ -40,13 +39,12 @@ class YaUploader:
         if response.status_code == 201:
             return f'File "{self.adr_parser(file_path)}" was created successfully'
 
-    def link_upload(self, src, disk_file_path):
+    def link_upload(self, url, disk_file_path):
+        """Метод загружает файл по url на Я.Диск по адресу disk_file_path"""
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        # like_name = VkUser.decomposer('profile')
-        params = {'path': disk_file_path, 'url': src, 'disable_redirects': 'false'}
+        params = {'path': disk_file_path, 'url': url, 'disable_redirects': 'false'}
         response = requests.post(upload_url, headers=headers, params=params)
-        # print(response)
         response.raise_for_status()
         if response.status_code == 202:
             return f'File was created successfully'
@@ -55,11 +53,11 @@ class YaUploader:
 class VkUser:
     url = 'https://api.vk.com/method/'
 
-    def __init__(self, token, version):
-        self.token = token
+    def __init__(self, token_vk, version):
+        self.token = token_vk
         self.version = version
         self.params = {
-            'access_token': self.token,
+            'access_token': self.token_vk,
             'v': self.version
         }
         self.owner_id = requests.get(self.url + 'users.get', self.params).json()['response'][0]['id']
@@ -85,7 +83,7 @@ class VkUser:
             'owner_id': owner_id,
             'album_id': album_id,
             'extended': 1,
-            'count': 1000
+            'count': 10
         }
         photo_info = requests.get(photo_url, params={**self.params, **photo_params})
         return photo_info.json()['response']['items']
@@ -98,20 +96,32 @@ class VkUser:
             attrib = {
                 'likes': foto['likes']['count'],
                 'size': foto['sizes'][-1]['type'],
-                'src': foto['sizes'][-1]['url']
+                'url': foto['sizes'][-1]['url']
             }
             attrib_list.append(attrib)
         sorted_by_likes_list = sorted(attrib_list, key=lambda i: i['likes'], reverse=True)
         return sorted_by_likes_list
 
 
+class Cyclone(YaUploader, VkUser):
+
+    def cycle_upload(self, name):
+        """Метод перебирает в цикле фото из VkUser.decomposer и передает в YaUploader.link_upload
+        имя = foto['likes']['count'], адрес = foto['sizes'][-1]['url']"""
+        photos_cycle = self.decomposer(name)
+        for photo in photos_cycle:
+            return photo['likes'], photo['url']
+
+
 if __name__ == '__main__':
-    uploader = YaUploader('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
-    # result = uploader.upload("D:\Python\Disk_file.txt")
-    result = uploader.link_upload('https://sun9-54.userapi.com/impf/HJ0UJYDpADxp1CKD-wmZ8DsEPBugvgTot1smKA/cbmFvAo3bhM.jpg?size=600x797&quality=96&sign=b1034a89f34892cc6b8b139e638e0c24&c_uniq_tag=6g8QxdJtyf5OKfuff460o5VIksDQLyjAhqQOSDooY6g&type=album', '/Neto/50.jpg')
-    # vk_client = VkUser('300f6b5d1cfe5dcd44fe9346ff2ce2efc37128d84986afffc85797438a6ff48402983dcb75bcba4501257', '5.130')
+    # uploader = YaUploader('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
+    # result = uploader.link_upload('https://sun9-39.userapi.com/c10472/u2571705/-6/w_75259ccf.jpg', '/Neto/50.jpg')
+    vk_client = VkUser('592a8b69055f08de859b8cd0a54bb73edc8aa96807c416e33a3d2b027adbab379bb0c6f100ee53cee82b0', '5.130')
     # print(vk_client.decomposer('profile'))
+    cycle = Cyclone('AQAAAAA36m8ZAADLW6XIsrMVfk9ImIKjJD3zTy0')
+    cycle.cycle_upload('profile')
+    print(cycle)
 
 # 'a7885b2a0024c9e139618ca0ff5be5688dd12f285dcb522ccfe6f0eacc76b59ac5fd6ddc2a89e354acfa0' frunde token
-#  '300f6b5d1cfe5dcd44fe9346ff2ce2efc37128d84986afffc85797438a6ff48402983dcb75bcba4501257' photo token
-# https://oauth.vk.com/authorize?client_id=7845912&display=page&scope=photos&response_type=token&v=5.130
+#  '592a8b69055f08de859b8cd0a54bb73edc8aa96807c416e33a3d2b027adbab379bb0c6f100ee53cee82b0' photo token
+# https://oauth.vk.com/authorize?client_id=7845912&display=page&scope=photos&expires_in=86400&response_type=token&v=5.130
